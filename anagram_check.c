7e94b81febcbde88f22e3e3941cd36fb59494562
JeonJaeWook <jetacore9219@gmail.com>
2019-06-04 05:20:50 +0900

    Create anagram_check.c

---
+++ anagram_check.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+int check_anagram(char[], char[]);
+int main()
+{
+	char a[100], b[100];
+	int flag;
+
+	printf("Enter first string\n");
+	gets_s(a);
+
+	printf("Enter second string\n");
+	gets_s(b);
+
+	flag = check_anagram(a, b);
+
+	printf(" %d \n",flag);
+	if (flag > 0)
+		printf("\"%s\" and \"%s\" are anagrams.\n", a, b);
+	else
+		printf("\"%s\" and \"%s\" are not anagrams.\n", a, b);
+
+	return 0;
+}
+
+int check_anagram(char a[], char b[])
+{
+	int first[26] = { 0 }, second[26] = { 0 }, a_c = 0, c=0,result = 0;
+	int a_number = strlen(a);
+	int b_number = strlen(b);
+	int count = 0;
+	bool comp = true;
+
+	while (b[c] != '\0')
+	{
+		second[b[c] - 'a']++;
+		c++;
+	}
+	c = 0;
+
+	do
+	{
+		for (int i = 0; i < b_number; i++)
+		{
+			first[a[c+i] - 'a']++;
+		}
+
+		for (int i = 0; i < 26; i++)
+		{
+			if (first[i] != second[i])
+			{
+				comp = false;
+			}
+		}
+		if (comp) result++;
+		comp = true;
+		c++;
+		for (int i = 0; i < 26; i++)
+		{
+			first[i] = 0;
+		}
+		a_number--;
+	} while (a_number - b_number >= 0);
+	return result;
+}
